f0c1fba651280022267a3c459790e20b726a978f
didimini <114174690+didimini@users.noreply.github.com>
2022-10-03 23:58:57 +0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+const schemeSvg = document.querySelector('.scheme-svg')
+const totalPriceTag = document.querySelector('.price-total')
+const menuButton = document.querySelector('.m-menu')
+const menu = document.querySelector('.menu')
+
+let cost = 800;
+let totalPrice = 0;
+
+schemeSvg.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('booked')) {
+        event.target.classList.toggle('active');
+        let totalSeat = schemeSvg.querySelectorAll('.active').length;
+        totalPrice = totalSeat * cost;
+        totalPriceTag.textContent = totalPrice;
+    }
+})
+
+menuButton.addEventListener('click', () => {
+    console.log("ejfhhefh");
+    menu.classList.toggle("is-open")
+})
