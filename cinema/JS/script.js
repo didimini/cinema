73605c9bff9c5b08b522074fadbb4fc248ca9c65
didimini <114174690+didimini@users.noreply.github.com>
2022-10-03 23:52:18 +0500

    Add files via upload

--- cinema/JS/script.js
+++ cinema/JS/script.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector('.scheme-svg')
 const totalPriceTag = document.querySelector('.price-total')
+const menuButton = document.querySelector('.m-menu')
+const menu = document.querySelector('.menu')
 
 let cost = 800;
 let totalPrice = 0;
@@ -11,4 +13,9 @@ schemeSvg.addEventListener('click', (event) => {
         totalPrice = totalSeat * cost;
         totalPriceTag.textContent = totalPrice;
     }
+})
+
+menuButton.addEventListener('click', () => {
+    console.log("ejfhhefh");
+    menu.classList.toggle("is-open")
 })
